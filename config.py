6c2647763e274a21dd1776551542cdc05b1680b9
IsenteBlackwell <143473548+IsenteBlackwell@users.noreply.github.com>
2023-08-29 11:41:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cvtphaopa6os.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limwenyuan-employee"
+customregion = "us-east-1"
+
